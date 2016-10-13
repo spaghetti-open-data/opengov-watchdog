3b607386d87be07da2cdf13b4d3e9071c8d2f345
Andrea Borruso <aborruso@gmail.com>
2016-10-13 11:26:25 +0200

    download e conversione in markdown di tutte le azioni della consultazione

---
+++ terzo-action-plan-italia/rawdata/consultazione/README.md
@@ -0,0 +1,4 @@
+Questa cartella contiene tutte le azioni elencate nalla pagina della "[Consultazione sul Terzo Piano d’Azionene](http://open.gov.it/partecipa/consultazioni-attive/consultazione-terzo-nap/)", convertite da HTML in markdown.
+
+Lo script usato (in Bash) per il download e la conversione in md è contenuto sempre in questa cartella e si chiama `getAzioni.sh`. Lo script sfrutta la lista di URL delle pagine delle azioni contenuto nel file `lista.txt`
+
---
+++ terzo-action-plan-italia/rawdata/consultazione/getAzioni.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### requirements ###
+# pandoc
+# scrape https://github.com/jeroenjanssens/data-science-at-the-command-line/blob/master/tools/scrape
+### requirements ###
+
+set -xv
+
+# imposto cartella di output
+cartella="/var/evvivaBrunati"
+
+# imposto il file che contiene gli URL cone le pagine delle azioni
+filename='/var/evvivaBrunati/lista.txt'
+
+# per ogni pagina listata nel file, creo un file markdowd per ogni azione
+echo Start
+i=1
+echo $i
+while read p; do 
+    #echo $p
+    curl -s "$p" | /usr/local/bin/nadir/scrape -be "//div[@class='entry-content']/*[not(name()='table')][position()>1 and position()<last()]" | pandoc -s -r html -o "$cartella/$i.md"
+	let i=$i+1
+	echo $i
+done < $filename
+
